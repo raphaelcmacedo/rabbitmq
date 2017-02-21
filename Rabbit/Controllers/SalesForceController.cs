cf0f32ed1f2134c1c815aec4f2511eff12fd0c49
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-02-21 09:19:31 -0300

    Backup

---
+++ Rabbit/Controllers/SalesForceController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rabbit.Controllers
+{
+    public class SalesForceController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        public void Listen(string queue, bool durable)
+        {
+            Queue.Receive.CreateListener(queue, durable);
+        }
+
+        public void Unlisten(bool durable)
+        {
+            Queue.Receive.listen = false;
+        }
+
+        public ActionResult Fetch(bool durable)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+                    string message = Queue.Receive.message;
+                    Queue.Receive.message = string.Empty;
+                    return Json(new { Success = true, data = message }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Success = false, Message = e.Message });
+                }
+
+            }
+
+            return View();
+        }
+
+        public ActionResult IntegrateOneMessage(string queue, bool durable)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(queue))
+                    {
+                        queue = "ha.salesorder.queue";
+                    }
+
+                    //Busca a msg no Rabbit
+                    string message = Queue.Receive.GetOneMessage(durable, queue, false, false);
+
+                    //Manda a msg para o salesforce
+
+                    return Json(new { Success = true, data = message }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Success = false, Message = e.Message });
+                }
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Rabbit/Scripts/views/salesForce.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    var messages = [];
+    setInterval(fetch, 2000);
+    
+    //Fetch 1 Message
+    $("#btnIntegrate").click(function (e) {
+        var durable = $("#durable").is(":checked");
+        var queue = $("#queueName").val();
+
+        $.ajax({
+            url: "SalesForce/IntegrateOneMessage",
+            type: 'POST',
+            data: { queue: queue, durable:durable },
+            success: function (object) {
+                if (object.Success) {
+                    var message = object.data;
+                    var queue = $("#queue").val();
+
+                    if (queue.length > 0) {
+                        queue += "\r\n";
+                    }
+
+                    queue += message;
+                    $("#queue").val(queue);
+                } else {
+                    alert(object.Message);
+                }
+
+            }
+        });
+    });
+
+});

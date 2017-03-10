1db931f25781147e23b0e14b1a1cab56dc39569f
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-03-10 11:06:52 -0300

    Opportunity no rabbit mq

--- Queue/Opportunity/ReceiveOpportunity.cs
+++ Queue/Opportunity/ReceiveOpportunity.cs
@@ -37,9 +37,11 @@ namespace Queue.Opportunity
                     SendOpportunity send = new SendOpportunity();
                     send.Send(opportunityMessage);
 
+                    //Send ack to queue
+                    //channel.BasicAck(ea.DeliveryTag, false);
                 };
                 channel.BasicConsume(queue: queue,
-                                        noAck: true,
+                                        noAck: false,
                                         consumer: consumer);
 
                 while (listen)
@@ -47,6 +49,43 @@ namespace Queue.Opportunity
                 
             }
         }
-        
+
+        public void CreateOpportunityListener()
+        {
+            listen = true;
+            string queue = "ha.bwopportunity.queue";
+            var factory = new ConnectionFactory() { HostName = "DV0219", UserName = "queue_user", Password = "testing1", VirtualHost = "qa" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: queue,
+                                        durable: true,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    //Get opportunity message
+                    string message = Encoding.UTF8.GetString(body);
+                    //Send opportunity to sales force
+                    Main.Services.OpportunityIntegration integration = new Main.Services.OpportunityIntegration();
+                    integration.CreateSalesForceOpportunity(message);
+                    
+                    //Send ack to queue
+                    //channel.BasicAck(ea.DeliveryTag, false);
+                };
+                channel.BasicConsume(queue: queue,
+                                        noAck: false,
+                                        consumer: consumer);
+
+                while (listen)
+                { }
+
+            }
+        }
+
     }
 }

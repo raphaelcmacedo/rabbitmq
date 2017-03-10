3a18642c915373c4c8e4c79fc728eb2279c313c9
Pedro Albani <pedroalbani@gmail.com>
2017-03-10 17:33:36 -0300

    backup

--- SalesForce/Services/OpportunityIntegration.cs
+++ SalesForce/Services/OpportunityIntegration.cs
@@ -77,6 +77,7 @@ namespace Main.Services
                     opportunity.SalesForceID = parentId;
                     repository.Update(opportunity);
                 }
+
             }
 
         }
